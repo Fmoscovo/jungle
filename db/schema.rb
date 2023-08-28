596ab6b88a3cba059974a56f82ea67509e2d86cb
Filipe <moscovo.filipe@gmail.com>
2023-08-28 09:59:53 -0600

    Set up RSpec and created sample widget tests

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_08_24_063845) do
+ActiveRecord::Schema.define(version: 2023_08_25_043932) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -63,6 +63,7 @@ ActiveRecord::Schema.define(version: 2023_08_24_063845) do
     t.boolean "active"
     t.date "start_date"
     t.date "end_date"
+    t.integer "status"
   end
 
   create_table "users", force: :cascade do |t|
